1580c4eb20a90609f11d3e6513f3e3b1215d437f
Alateon <dc.shoes@hotmail.fr>
2016-03-27 23:22:25 +0200

    Creation des bases du serveur NodeJs

---
+++ server.js
@@ -0,0 +1,59 @@
+/*var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+        res.write('Vous devez bien avoir un prénom et un nom, non ?');
+    }
+    res.end();
+});
+
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
+
+app.listen(8000);
+*/
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var passport	= require('passport');
+var config      = require('./config/database'); // get db config file
+var User        = require('./app/models/user'); // get the mongoose model
+var port        = process.env.PORT || 8000;
+var jwt         = require('jwt-simple');
+ 
+// get our request parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
+// log to console
+app.use(morgan('dev'));
+ 
+// Use the passport package in our application
+app.use(passport.initialize());
+ 
+// demo Route (GET http://localhost:8080)
+app.get('/', function(req, res) {
+  res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+ 
+// Start the server
+app.listen(port);
+console.log('There will be dragons: http://localhost:' + port);
